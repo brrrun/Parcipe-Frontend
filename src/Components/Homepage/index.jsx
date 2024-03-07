93368b100e71fca134fe9e24d6250c5829ed66ff
brrrun <brunomsantosmendes@gmail.com>
2024-03-07 22:10:12 +0000

    Homepage test

--- src/Components/Homepage/index.jsx
+++ src/Components/Homepage/index.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import "./styles.css"
 
-const API_URL = "hhttps://parcipe-backend.onrender.com"
+const API_URL = "hhttps://parcipe-backend-phi.vercel.app"
 
 function Homepage() {
 
